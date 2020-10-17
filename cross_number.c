62705654efa63d62b84efac2d8c2b0839523f78c
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-10-18 00:09:04 +0530

    Add files via upload

---
+++ cross_number.c
@@ -0,0 +1,34 @@
+/*
+code for below pattern
+* * * * *  * * * * *
+* * * *      * * * *
+* * *          * * *
+* *              * *
+*                  *
+*                  *
+* *              * *
+* * *          * * *
+* * * *      * * * *
+* * * * *  * * * * *
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,k=1,j;
+
+   for( i=1;i<=10;i++)
+    {
+        for(int j=1;j<=11;j++)
+        {
+            printf(" ");
+            if(j<=6-i||j>=6+i ||j<=i-5||j>=17-i)
+            printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
