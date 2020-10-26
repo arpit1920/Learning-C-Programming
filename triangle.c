f696174fe2da4bff467ea44bf9f1ad2f3d0952cc
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-10-27 00:38:41 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,26 @@
+/*
+Half Pyramid
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,k=1,j;
+    for( i=0;i<=4;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            if(j<=i)
+            printf(" *");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
