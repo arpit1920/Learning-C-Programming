0aff6ae55e197b1f134b821b02144c30aef3e428
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2021-01-01 10:08:56 +0530

    Add files via upload

--- addition_of_no.c
+++ addition_of_no.c
@@ -1,16 +1,15 @@
-//28)	To add two numbers without using addition operator.
-#include<stdio.h>
-#include<stdlib.h>
+//To add two numbers without using addition operator
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int m,n,result;
+    int m, n, result;
     printf("Enter first and second number:\n");
-    scanf("%d\n%d",&m,&n);
-    while(n!=0)
-    {
+    scanf("%d\n%d", &m, &n);
+    while (n != 0) {
         m++;
         n--;
     }
-    printf("Addition is:%d",m);
+    printf("Addition is:%d", m);
     return 0;
 }
